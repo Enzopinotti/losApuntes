7b909175e416ae234e5f923ceaeccd8696db976e
Enzo Pinotti <enzopinottii@gmail.com>
2026-09-23 02:20:33 -0300

    fix(resources): normalize metadata search escaping

--- apps/api/src/resources/mongo/mongo-resource.store.ts
+++ apps/api/src/resources/mongo/mongo-resource.store.ts
@@ -37,7 +37,7 @@ function toPlain<T>(value: { toObject(): unknown } | T): T {
 }
 
 function escapeRegex(value: string): string {
-  return value.replace(/[.*+?^\${}()|[\]\\]/gu, '\\$&');
+  return value.replace(/[.*+?^${}()|[\]\\]/gu, '\\return value.replace(/[.*+?^\${}()|[\]\\]/gu, '\\$&');');
 }
 
 @Injectable()
